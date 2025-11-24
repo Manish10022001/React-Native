e8262de8a274f1b80833063087f3313def70b1d6
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-25 03:34:17 +0530

    fix Dimensions API drawback and support responsive layout on orientation change

--- react_native_js/App.js
+++ react_native_js/App.js
@@ -1,11 +1,32 @@
 // import One from "./src/examples/One.js";
+import {useState, useEffect} from 'react';
 import PressableComponent from "./src/examples/PressableComponent.js";
 import AlertComponent from "./src/components/AlertComponent.js";
 import StyleInheritance from "./src/StylesSheet/StyleInheritance.js";
 import { View, Text , StyleSheet, Dimensions } from "react-native";
 import Box from "./src/RNLayout/Box.js";
-
+//drawback of Dimensions Api, when we change screen to landscape we need to again reload the app, the values don't dynamically update when screen changes to resolve that we have to follow 5 steps
+//  step1: import useState and useEffect from react
 export default function App() {
+  //step : create a state variable that will store device dimensions when screen loads
+  const [dimensions, setDimensions] = useState({
+    //pass object and will have key called window
+    window: Dimensions.get('window')
+  })
+
+  //step 3: add effect that listens to changes in device dimensions and update dimension to store value
+  useEffect(()=>{
+    const subscription = Dimensions.addEventListener("change",({window})=>{
+      setDimensions({window})
+    });
+    return ()=> subscription?.remove();
+  })
+
+  //step 4 : extract device width and height from dimensions state variable
+  const {window} = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+  //step 5: add inline styles that dynamically change based on device dimensions
   return (
     <View style={styles.container}>
       {/* <One /> */}
@@ -36,8 +57,10 @@ export default function App() {
          <Box style={{ backgroundColor:"#5c85c6ff" }}>Box 7</Box> */}
       {/*</View>*/}
 
-        <View style={styles.box}>
-          <Text style={styles.text}>Welcome !</Text>
+        <View style={[styles.box,{
+          width:windowWidth>500?'70%':'90%',
+           height:windowHeight>500?'70%':'90%'}]}>
+          <Text style={{fontSize:windowWidth>500?74:20}}>Welcome !</Text>
         </View>
       </View>
     
@@ -55,13 +78,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box:{
-    width:windowWidth > 500 ? '50%' : '70%',
-    height:windowHeight > 500 ? '50%':'70%',
+    // width:windowWidth > 500 ? '50%' : '70%',
+    // height:windowHeight > 500 ? '50%':'70%',
     backgroundColor:'lightblue',
     alignItems:'center',
     justifyContent:'center'
   },
-  text:{
-    fontSize:windowWidth>500? 54: 24,
-  }
+  // text:{
+  //   fontSize:windowWidth>500? 54: 24,
+  // }
 });
