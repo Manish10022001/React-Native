070699a2b2070e75dc46c446c025d2ba26bdd642
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-25 02:54:16 +0530

    Dynamic UI: dimentions API

--- react_native_js/App.js
+++ react_native_js/App.js
@@ -2,61 +2,66 @@
 import PressableComponent from "./src/examples/PressableComponent.js";
 import AlertComponent from "./src/components/AlertComponent.js";
 import StyleInheritance from "./src/StylesSheet/StyleInheritance.js";
-import { View, Text , StyleSheet} from "react-native";
+import { View, Text , StyleSheet, Dimensions } from "react-native";
 import Box from "./src/RNLayout/Box.js";
 
 export default function App() {
   return (
-    // <One />
-    // <JsxExampleCat />
-    // <CatProps />
-    // <ImageProps />
-    // <CatState />
-    // <PizzaTranslator />
-    // <ScrollViewEx />
-    // <ButtonComponent />
-    // <PressableComponent />
-    // <ModalComponent />
-    // <StatusBarComponent />
-    // <ActivityIndicatorComponent />
-    // <Alert />
-    // <MultipleStyles />
-    // <BoxModel />
-    // <ExampleShadow />
-    // <StyleInheritance />
     <View style={styles.container}>
-      <Box style={{backgroundColor:"#681414ff", top:75, left:75}}>Box 1 </Box>
-      <Box style={{backgroundColor:"#cec36eff", top:78, left:75}}>Box 2</Box>
-      <Box style={{backgroundColor:"#d689c4ff", position:'absolute', top:100, left:100}}>Box 3</Box>
-      <Box style={{backgroundColor:"#2a0958ff"}}>Box 4</Box>
-      <Box style={{backgroundColor:"#21732fff"}}>Box 5</Box>
-      <Box style={{backgroundColor:"#0e0404ff"}}>Box 6</Box>
-      <Box style={{backgroundColor:"#5c85c6ff"}}>Box 7</Box>
-    </View>
+      {/* <One /> */}
+      {/* <JsxExampleCat /> */}
+      {/* <CatProps /> */}
+      {/* <ImageProps /> */}
+      {/* <CatState /> */}
+      {/* <PizzaTranslator /> */}
+      {/* <ScrollViewEx /> */}
+      {/* <ButtonComponent /> */}
+      {/* <PressableComponent /> */}
+      {/* <ModalComponent /> */}
+      {/* <StatusBarComponent /> */}
+      {/* <ActivityIndicatorComponent /> */}
+      {/* <Alert /> */}
+      {/* <MultipleStyles /> */}
+      {/* <BoxModel /> */}
+      {/* <ExampleShadow /> */}
+      {/* <StyleInheritance /> */}
+
+      {/* <View style={styles.container}>*/}
+         {/* <Box style={{ backgroundColor:"#681414ff", top:75, left:75 }}>Box 1</Box>
+         <Box style={{ backgroundColor:"#cec36eff", top:78, left:75 }}>Box 2</Box>
+         <Box style={{ backgroundColor:"#d689c4ff", position:'absolute', top:100, left:100 }}>Box 3</Box>
+         <Box style={{ backgroundColor:"#2a0958ff" }}>Box 4</Box>
+         <Box style={{ backgroundColor:"#21732fff" }}>Box 5</Box>
+         <Box style={{ backgroundColor:"#0e0404ff" }}>Box 6</Box>
+         <Box style={{ backgroundColor:"#5c85c6ff" }}>Box 7</Box> */}
+      {/*</View>*/}
+
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome !</Text>
+        </View>
+      </View>
+    
   );
 }
 
-const styles= StyleSheet.create({
-  container:{
-    flex:1,
-    flexDirection:"column",
-    // justifyContent:"flex-start",
-    // justifyContent:"flex-end",
-    // justifyContent:"center",
-    // justifyContent:"space-evenly",
-    // alignItems:"stretch" //by default
-    alignItems:"flex-start",
-    // alignItems:"flex-end",
-    // alignItems:"center",
-    // alignItems:"flex-end",
-    // flexWrap:'nowrap',
-    // flexWrap:'wrap',
-    // alignContent:"space-around", //other values, flex-start, flex-end, cente, space-between, stretch, etc
-    // flexWrap:"wrap",
-    // height:300,
-    width:300,
-    // marginTop:60,
-    // borderWidth:6,
-    // borderColor:"red",
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box:{
+    width:windowWidth > 500 ? '50%' : '70%',
+    height:windowHeight > 500 ? '50%':'70%',
+    backgroundColor:'lightblue',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  text:{
+    fontSize:windowWidth>500? 54: 24,
   }
-})
+});
