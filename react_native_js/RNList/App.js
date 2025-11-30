796368fe2a70a08c64ea8e54907f41fab4b96e65
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-30 20:36:21 +0530

    FlatList, recommended approach to render list

--- react_native_js/RNList/App.js
+++ react_native_js/RNList/App.js
@@ -1,25 +1,46 @@
-import { ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, StatusBar, FlatList } from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import PokemonList from "./data.json";
 
 export default function App() {
   return (
+    // <SafeAreaProvider>
+    //   <SafeAreaView style={styles.container}>
+    //     <ScrollView style={styles.scrollView}>
+    //       {
+    //         PokemonList.map((pokemon)=>{
+    //           return(
+    //             <View style={styles.card} key={pokemon.id}>
+    //               <Text style={styles.cardText}>{pokemon.name}</Text>
+    //               <Text style={styles.cardText}>{pokemon.type}</Text>
+    //             </View>
+    //           )
+    //         })
+    //       }
+    //     </ScrollView>
+    //   </SafeAreaView>
+    // </SafeAreaProvider>
+
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-          {
-            PokemonList.map((pokemon)=>{
-              return(
-                <View style={styles.card} key={pokemon.id}>
-                  <Text style={styles.cardText}>{pokemon.name}</Text>
-                  <Text style={styles.cardText}>{pokemon.type}</Text>
-                </View>
-              )
-            })
-          }
-        </ScrollView>
+       <SafeAreaView style={styles.container}>
+        <View style={styles.scrollView}>
+        <FlatList 
+          data={PokemonList}
+          renderItem={({item})=>{
+            console.log(item.id);
+            return(
+              <View style={styles.card}>
+                <Text>{item.type}</Text>
+                <Text>{item.name}</Text>
+              </View>
+            )
+          }}
+          // horizontal = {true}
+          keyExtractor={(item,index)=> item.id.toString()}
+        />
+        </View>
       </SafeAreaView>
-    </SafeAreaProvider>
+    </SafeAreaProvider> 
   );
 }
 
