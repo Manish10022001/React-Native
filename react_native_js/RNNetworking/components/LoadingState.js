73e93761cacaeb690ee817e281e281e287f12ee8
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-02 16:24:34 +0530

    react native networking : loading state added

--- react_native_js/RNNetworking/components/LoadingState.js
+++ react_native_js/RNNetworking/components/LoadingState.js
@@ -1,22 +1,33 @@
 import {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, StatusBar, FlatList } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, FlatList, ActivityIndicator } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 //1. Get Request and bind the response to ui
 //2. Add loading state while data is fetched in the background
-export default function GetRequests() {
+export default function LoadingState() {
     const [postList, setPostList] = useState([])
+    const [isLoading, setIsLoading]   = useState(true)
     //fetch api data using asyn function
     const fetchData = async(limit=10)=>{
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`)
         const data = await response.json();
         
         setPostList(data)
+        setIsLoading(false)
     }
 
     //get fetch data when component mounts
     useEffect(()=>{
         fetchData()
     },[])
+
+    if(isLoading){
+        return(
+            <SafeAreaView style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="#0000ff" />
+                <Text>...Loading</Text>
+            </SafeAreaView>
+        )
+    }
   return (
     // <SafeAreaProvider>
     //   <SafeAreaView style={styles.container}>
@@ -84,5 +95,12 @@ const styles = StyleSheet.create({
     fontSize:24,
     textAlign:"center",
     marginTop:12
+  },
+  loadingContainer:{
+    flex:1,
+    backgroundColor:"#f5f5f5",
+    paddingTop:StatusBar.currentHeight,
+    justifyContent:"center",
+    alignItems:"center"
   }
 });
