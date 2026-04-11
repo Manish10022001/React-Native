c78bf677dff91b4a8e018180c01fd386eb96982b
Manish10022001 <manishshirsat2850@gmail.com>
2026-04-11 20:13:36 +0530

    Stack Navigation : Stack Screen Options

--- react_native_ts/Expo_Router_Navigation/app/details/_layout.tsx
+++ react_native_ts/Expo_Router_Navigation/app/details/_layout.tsx
@@ -1,7 +1,17 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout(){
-    return(
-        <Stack />
-    )
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    />
+  );
 }
