0f5669f5df4070bee174d317042290a4baa75fad
Manish10022001 <manishshirsat2850@gmail.com>
2026-04-11 20:56:37 +0530

    Customize header and back button globally

--- react_native_ts/Expo_Router_Navigation/app/_layout.tsx
+++ react_native_ts/Expo_Router_Navigation/app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
+import { Button, Pressable, Text } from "react-native";
 
 export default function RootLayout() {
   return (
     <Stack
-      screenOptions={{
+      screenOptions={({ navigation }) => ({
         headerStyle: {
           backgroundColor: "red",
         },
@@ -11,7 +12,22 @@ export default function RootLayout() {
         headerTitleStyle: {
           fontWeight: "bold",
         },
-      }}
+        // to customize back button in header globally, use headerLeft in which render custom component
+        headerLeft: () => (
+          //   <Button
+          //     title="Back"
+          //     color={"blue"}
+          //     //need to give prop navigation to set condition to go back
+          //     onPress={() => navigation.goBack()}
+          //   />
+          <Pressable
+            onPress={() => navigation.goBack()}
+            style={{ marginRight: 10 }}
+          >
+            <Text style={{ color: "blue", fontSize: 16 }}>Back</Text>
+          </Pressable>
+        ),
+      })}
     >
       {/* //if we want to customise the title and other properties of the screen
       //individually we need to configure the options outside the root //using
@@ -20,7 +36,10 @@ export default function RootLayout() {
         name="routename"
         options={{ title: "Name we want to give" }}
       /> */}
-      <Stack.Screen name="index" options={{ title: "Home Page" }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Home Page", headerLeft: undefined }} //added headerLeft property to undefined to clear back button or array from home scrren
+      />
       <Stack.Screen name="setting" options={{ title: "Setting Page" }} />
     </Stack>
   );
